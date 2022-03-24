e8447ba251af36dc98591304f5b9f43a30557eb7
Steven Xu <stevenxxiu@gmail.com>
2022-03-24 20:12:00 +1100

    feat: add *Pre-commit* hooks; format `__init__.py`

--- __init__.py
+++ __init__.py
@@ -1,102 +1,108 @@
 # -*- coding: utf-8 -*-
 
-"""Query and install ArchLinux User Repository (AUR) packages.
+'''Query and install ArchLinux User Repository (AUR) packages.
 
 You can search for packages and open their URLs. This extension is also intended to be used to \
 quickly install the packages. If you are missing your favorite AUR helper tool send a PR.
 
-Synopsis: <trigger> <pkg_name>"""
+Synopsis: <trigger> <pkg_name>'''
 
-from albert import *
-from shutil import which
-from datetime import datetime
-from urllib import request, parse
 import json
 import os
 import re
+from datetime import datetime
+from shutil import which
+from urllib import parse, request
 
-__title__ = "Archlinux User Repository"
-__version__ = "0.4.3"
-__triggers__ = "aur "
-__authors__ = "manuelschneid3r"
+from albert import Item, TermAction, UrlAction  # pylint: disable=import-error
 
-iconPath = os.path.dirname(__file__)+"/arch.svg"
+
+__title__ = 'Archlinux User Repository'
+__version__ = '0.4.3'
+__triggers__ = 'aur '
+__authors__ = 'manuelschneid3r'
+
+icon_path = os.path.dirname(__file__) + '/arch.svg'
 baseurl = 'https://aur.archlinux.org/rpc/'
 install_cmdline = None
 
-if which("yaourt"):
-    install_cmdline = "yaourt -S aur/%s"
-elif which("pacaur"):
-    install_cmdline = "pacaur -S aur/%s"
-elif which("yay"):
-    install_cmdline = "yay -S aur/%s"
+if which('yaourt'):
+    install_cmdline = 'yaourt -S aur/%s'
+elif which('pacaur'):
+    install_cmdline = 'pacaur -S aur/%s'
+elif which('yay'):
+    install_cmdline = 'yay -S aur/%s'
+
+
+def entry_to_item(entry, query_pattern):
+    name = entry['Name']
+    item = Item(
+        id=__title__,
+        icon=icon_path,
+        text=(
+            f'<b>{query_pattern.sub(lambda m: f"<u>{m.group(0)}</u>", name)}</b> '
+            f'<i>{entry["Version"]}</i> ({entry["NumVotes"]})'
+        ),
+        completion=f'{__triggers__}{name}',
+    )
+    subtext = entry['Description'] if entry['Description'] else '[No description]'
+    if entry['OutOfDate']:
+        date_text = datetime.fromtimestamp(entry['OutOfDate']).strftime('%F')
+        subtext = f'<font color="red">[Out of date: {date_text}]</font> {subtext}'
+    if entry['Maintainer'] is None:
+        subtext = f'<font color="red">[Orphan]</font> {subtext}'
+    item.subtext = subtext
+
+    if install_cmdline:
+        pkgmgr = install_cmdline.split('' '', 1)
+        pkg_install_cmdline = install_cmdline % name
+        item.actions = [
+            TermAction(f'Install using {pkgmgr[0]}', pkg_install_cmdline),
+            TermAction(f'Install using {pkgmgr[0]} (noconfirm)', f'{pkg_install_cmdline} --noconfirm'),
+        ]
+
+    item.addAction(UrlAction('Open AUR website', f'https://aur.archlinux.org/packages/{name}/'))
+
+    if entry['URL']:
+        item.addAction(UrlAction('Open project website', entry['URL']))
+
 
 def handleQuery(query):
     if not query.isTriggered:
-        return
+        return None
 
     query.disableSort()
 
     stripped = query.string.strip()
 
-    if stripped:
-        params = {
-            'v': '5',
-            'type': 'search',
-            'by': 'name',
-            'arg': stripped
-        }
-        url = "%s?%s" % (baseurl, parse.urlencode(params))
-        req = request.Request(url)
+    if not stripped:
+        return Item(
+            id=__title__,
+            icon=icon_path,
+            text=__title__,
+            subtext='Enter a query to search the AUR',
+            actions=[UrlAction('Open AUR packages website', 'https://aur.archlinux.org/packages/')],
+        )
 
-        with request.urlopen(req) as response:
-            data = json.loads(response.read().decode())
-            if data['type'] == "error":
-                return Item(
-                    id=__title__,
-                    icon=iconPath,
-                    text="Error",
-                    subtext=data['error']
-                )
-            else:
-                results = []
-                pattern = re.compile(query.string, re.IGNORECASE)
-                results_json = data['results']
-                results_json.sort(key=lambda item: item['Name'])
-                results_json.sort(key=lambda item: len(item['Name']))
+    params = {'v': '5', 'type': 'search', 'by': 'name', 'arg': stripped}
+    url = f'{baseurl}?{parse.urlencode(params)}'
+    req = request.Request(url)
 
-                for entry in results_json:
-                    name = entry['Name']
-                    item = Item(
-                        id = __title__,
-                        icon = iconPath,
-                        text = "<b>%s</b> <i>%s</i> (%s)" % (pattern.sub(lambda m: "<u>%s</u>" % m.group(0), name), entry['Version'], entry['NumVotes']),
-                        completion = "%s%s" % (__triggers__, name)
-                    )
-                    subtext = entry['Description'] if entry['Description'] else "[No description]"
-                    if entry['OutOfDate']:
-                        subtext = '<font color="red">[Out of date: %s]</font> %s' % (datetime.fromtimestamp(entry['OutOfDate']).strftime("%F"), subtext)
-                    if entry['Maintainer'] is None:
-                        subtext = '<font color="red">[Orphan]</font> %s' % subtext
-                    item.subtext = subtext
+    with request.urlopen(req) as response:
+        data = json.loads(response.read().decode())
+        if data['type'] == 'error':
+            return Item(
+                id=__title__,
+                icon=icon_path,
+                text='Error',
+                subtext=data['error'],
+            )
+        results = []
+        query_pattern = re.compile(query.string, re.IGNORECASE)
+        results_json = data['results']
+        results_json.sort(key=lambda item: item['Name'])
+        results_json.sort(key=lambda item: len(item['Name']))
 
-                    if install_cmdline:
-                        pkgmgr = install_cmdline.split(" ", 1)
-                        item.actions = [
-                            TermAction("Install using %s" % pkgmgr[0], install_cmdline % name),
-                            TermAction("Install using %s (noconfirm)" % pkgmgr[0], install_cmdline % name + " --noconfirm")                        
-                        ]
-
-                    item.addAction(UrlAction("Open AUR website", "https://aur.archlinux.org/packages/%s/" % name))
-
-                    if entry['URL']:
-                        item.addAction(UrlAction("Open project website", entry['URL']))
-
-                    results.append(item)
-                return results
-    else:
-        return Item(id=__title__,
-                    icon=iconPath,
-                    text=__title__,
-                    subtext="Enter a query to search the AUR",
-                    actions=[UrlAction("Open AUR packages website", "https://aur.archlinux.org/packages/")])
+        for entry in results_json:
+            results.append(entry_to_item(entry, query_pattern))
+        return results
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Arch Packages Search Extension
+# Install
+To install, copy or symlink this directory to `~/.local/share/albert/org.albert.extension.python/modules/arch_packages/`.
+
+# Development Setup
+To setup the project for development, run:
+
+    $ cd arch_packages/
+    $ npm install
+    $ pre-commit install --hook-type pre-commit --hook-type commit-msg
