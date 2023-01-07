4af4183c56b17cf8a2eb172df80fb08ecd7e3362
Steven Xu <stevenxxiu@gmail.com>
2023-01-07 22:29:33 +1100

    refactor: migrate to *Albert Launcher* v0.18.6

--- __init__.py
+++ __init__.py
@@ -1,32 +1,32 @@
-'''Query *Arch Linux* official and *AUR* packages.
-
-You can search for packages and open their URLs.
-
-Synopsis: <trigger> <pkg_name>'''
-
 import concurrent.futures
 import json
 import re
+import time
 from datetime import datetime, timezone
 from pathlib import Path
+from re import Pattern
+from typing import List
 from urllib import parse, request
 
-from albert import Item, UrlAction  # pylint: disable=import-error
+from albert import Action, Item, Query, QueryHandler, openUrl  # pylint: disable=import-error
 
 
-__title__ = 'Arch Linux Packages'
-__version__ = '0.4.4'
-__triggers__ = 'apkg '
-__authors__ = ['Steven Xu', 'manuelschneid3r']
+md_iid = '0.5'
+md_version = '1.0'
+md_name = 'Arch Linux Packages'
+md_description = 'Query *Arch Linux* official and *AUR* packages. Search for packages, open their URLs.'
+md_url = 'https://github.com/stevenxxiu/albert_arch_packages'
+md_maintainers = '@stevenxxiu'
 
+TRIGGER = 'apkg'
 ICON_PATH = str(Path(__file__).parent / 'icons/arch.svg')
 
 
-def to_local_time_str(datetime_obj):
+def to_local_time_str(datetime_obj: datetime) -> str:
     return datetime_obj.replace(tzinfo=timezone.utc).astimezone().strftime('%F %T')
 
 
-def highlight_query(query_pattern, name):
+def highlight_query(query_pattern: Pattern, name: str) -> str:
     return query_pattern.sub(lambda m: f'<u>{m.group(0)}</u>', name)
 
 
@@ -34,14 +34,9 @@ class ArchOfficialRepository:
     API_URL = 'https://www.archlinux.org/packages/search/json'
 
     @staticmethod
-    def entry_to_item(entry, query_pattern):
-        name = entry['pkgname']
-        item = Item(
-            id=__title__,
-            icon=ICON_PATH,
-            text=f'<b>{highlight_query(query_pattern, name)}</b> <i>{entry["pkgver"]}-{entry["pkgrel"]}</i>',
-            completion=f'{__triggers__}{name}',
-        )
+    def entry_to_item(entry: dict, query_pattern: Pattern) -> Item:
+        name: str = entry['pkgname']
+
         subtext = entry['pkgdesc']
         if entry['flag_date']:
             date_text = to_local_time_str(datetime.strptime(entry['flag_date'], '%Y-%m-%dT%H:%M:%S.%fZ'))
@@ -49,31 +44,34 @@ class ArchOfficialRepository:
         if not entry['maintainers']:
             subtext = f'<font color="red">[Orphan]</font> {subtext}'
         subtext = f'<font color="dimgray">[{entry["repo"]}]</font> {subtext}'
-        item.subtext = subtext
 
-        item.addAction(
-            UrlAction(
-                'Open Arch repositories website',
-                f'https://archlinux.org/packages/{entry["repo"]}/{entry["arch"]}/{name}/',
+        url = f'https://archlinux.org/packages/{entry["repo"]}/{entry["arch"]}/{name}/'
+        actions = [Action(f'{md_name}/{url}', 'Open Arch repositories website', lambda: openUrl(url))]
+        if entry['url']:
+            actions.append(
+                Action(f'{md_name}/{entry["url"]}', 'Open project website', lambda: openUrl(entry['url'])),
             )
+
+        return Item(
+            id=f'{md_name}/{entry["repo"]}/{entry["arch"]}/{name}',
+            text=f'{highlight_query(query_pattern, name)} {entry["pkgver"]}-{entry["pkgrel"]}',
+            subtext=subtext,
+            completion=f'{TRIGGER} {name}',
+            icon=[ICON_PATH],
+            actions=actions,
         )
 
-        if entry['url']:
-            item.addAction(UrlAction('Open project website', entry['url']))
-
-        return item
-
     @classmethod
-    def query(cls, query_str):
-        repos = ['Core', 'Extra', 'Community']
-        repos_lower = [repo.lower() for repo in repos]
-        params = [('repo', repo) for repo in repos] + [('q', query_str)]
+    def query(cls, query_str: str) -> List[Item]:
+        repos: List[str] = ['Core', 'Extra', 'Community']
+        repos_lower: List[str] = [repo.lower() for repo in repos]
+        params: List[tuple[str, str]] = [('repo', repo) for repo in repos] + [('q', query_str)]
         url = f'{cls.API_URL}?{parse.urlencode(params)}'
         req = request.Request(url)
 
         with request.urlopen(req) as response:
             data = json.loads(response.read().decode())
-            results = []
+            items: List[Item] = []
             results_json = data['results']
             results_json.sort(
                 key=lambda entry_: (repos_lower.index(entry_["repo"]), len(entry_['pkgname']), entry_['pkgname'])
@@ -86,22 +84,17 @@ class ArchOfficialRepository:
                 # https://wiki.archlinux.org/title/Official_repositories_web_interface.
                 if query_str not in entry['pkgname']:
                     continue
-                results.append(cls.entry_to_item(entry, query_pattern))
-            return results
+                items.append(cls.entry_to_item(entry, query_pattern))
+            return items
 
 
 class ArchUserRepository:
     API_URL = 'https://aur.archlinux.org/rpc/'
 
     @staticmethod
-    def entry_to_item(entry, query_pattern):
+    def entry_to_item(entry: dict, query_pattern: Pattern) -> Item:
         name = entry['Name']
-        item = Item(
-            id=__title__,
-            icon=ICON_PATH,
-            text=f'<b>{highlight_query(query_pattern, name)}</b> <i>{entry["Version"]}</i> ({entry["NumVotes"]})',
-            completion=f'{__triggers__}{name}',
-        )
+
         subtext = f'{entry["Description"] if entry["Description"] else "[No description]"}'
         if entry['OutOfDate']:
             date_text = to_local_time_str(datetime.fromtimestamp(entry['OutOfDate']))
@@ -109,17 +102,25 @@ class ArchUserRepository:
         if entry['Maintainer'] is None:
             subtext = f'<font color="red">[Orphan]</font> {subtext}'
         subtext = f'<font color="dimgray">[AUR]</font> {subtext}'
-        item.subtext = subtext
 
-        item.addAction(UrlAction('Open AUR website', f'https://aur.archlinux.org/packages/{name}/'))
-
+        url = f'https://aur.archlinux.org/packages/{name}/'
+        actions = [Action(f'{md_name}/{url}', 'Open AUR website', lambda: openUrl(url))]
         if entry['URL']:
-            item.addAction(UrlAction('Open project website', entry['URL']))
+            actions.append(
+                Action(f'{md_name}/{entry["URL"]}', 'Open project website', lambda: openUrl(entry['URL'])),
+            )
 
-        return item
+        return Item(
+            id=f'{md_name}/AUR/{name}',
+            text=f'<b>{highlight_query(query_pattern, name)}</b> <i>{entry["Version"]}</i> ({entry["NumVotes"]})',
+            subtext=subtext,
+            completion=f'{TRIGGER} {name}',
+            icon=[ICON_PATH],
+            actions=actions,
+        )
 
     @classmethod
-    def query(cls, query_str):
+    def query(cls, query_str: str) -> List[Item]:
         params = {'v': '5', 'type': 'search', 'by': 'name', 'arg': query_str}
         url = f'{cls.API_URL}?{parse.urlencode(params)}'
         req = request.Request(url)
@@ -127,50 +128,79 @@ class ArchUserRepository:
         with request.urlopen(req) as response:
             data = json.loads(response.read().decode())
             if data['type'] == 'error':
-                return Item(
-                    id=__title__,
-                    icon=ICON_PATH,
-                    text='Error',
-                    subtext=data['error'],
-                )
-            results = []
+                return [
+                    Item(
+                        id=f'{md_name}/aur_error',
+                        text='Error',
+                        subtext=data['error'],
+                        icon=[ICON_PATH],
+                    )
+                ]
             results_json = data['results']
             results_json.sort(key=lambda entry_: (len(entry_['Name']), entry_['Name']))
 
             query_pattern = re.compile(query_str, re.IGNORECASE)
-            for entry in results_json:
-                results.append(cls.entry_to_item(entry, query_pattern))
-            return results
+            items = [cls.entry_to_item(entry, query_pattern) for entry in results_json]
+            return items
 
 
-def handleQuery(query):
-    if not query.isTriggered:
-        return None
+class Plugin(QueryHandler):
+    @staticmethod
+    def id() -> str:
+        return __name__
 
-    query.disableSort()
+    @staticmethod
+    def name() -> str:
+        return md_name
 
-    query_str = query.string.strip()
-    if not query_str:
-        return [
-            Item(
-                id=__title__,
-                icon=ICON_PATH,
-                text=__title__,
+    @staticmethod
+    def description() -> str:
+        return md_description
+
+    @staticmethod
+    def defaultTrigger() -> str:
+        return TRIGGER
+
+    @staticmethod
+    def synopsis() -> str:
+        return 'pkg_name'
+
+    @staticmethod
+    def handleQuery(query: Query) -> None:
+        query_str = query.string.strip()
+        if not query_str:
+            item = Item(
+                id=f'{md_name}/open_websites',
+                text=md_name,
                 subtext='Enter a query to search Arch Linux and AUR packages',
+                icon=[ICON_PATH],
                 actions=[
-                    UrlAction('Open Arch repositories website', 'https://archlinux.org/packages/'),
-                    UrlAction('Open AUR website', 'https://aur.archlinux.org/packages/'),
+                    Action(
+                        f'{md_name}/open_arch_repos',
+                        'Open Arch repositories website',
+                        lambda: openUrl('https://archlinux.org/packages/'),
+                    ),
+                    Action(
+                        f'{md_name}/open_aur',
+                        'Open AUR website',
+                        lambda: openUrl('https://aur.archlinux.org/packages/'),
+                    ),
                 ],
-            ),
-        ]
+            )
+            query.add(item)
+            return
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [
-            executor.submit(ArchOfficialRepository.query, query_str),
-            executor.submit(ArchUserRepository.query, query_str),
-        ]
-        results = []
-        concurrent.futures.wait(futures)
-        for future in futures:
-            results.extend(future.result())
-        return results
+        # Avoid rate limiting
+        time.sleep(0.2)
+        if not query.isValid:
+            return
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(ArchOfficialRepository.query, query_str),
+                executor.submit(ArchUserRepository.query, query_str),
+            ]
+            concurrent.futures.wait(futures)
+            for future in futures:
+                for item in future.result():
+                    query.add(item)
